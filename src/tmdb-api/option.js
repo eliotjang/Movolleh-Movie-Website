585ace7fe8470ab5a67f8b8d862626856291e733
KAPUIST <91464689+KAPUIST@users.noreply.github.com>
2024-05-01 17:48:29 +0900

    rollback

---
+++ src/tmdb-api/option.js
@@ -0,0 +1,8 @@
+export const options = {
+  method: 'GET',
+  headers: {
+    accept: 'application/json',
+    Authorization:
+      'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyOWQ3YWI1OWZmZTc5YmQyODA5NTRhMjQ4M2QzOWUxZSIsInN1YiI6IjYxYWIxYjkwNmMxZTA0MDA0MmI2Yzg0NyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ._rYfZK7OtFsmskyV66wMO10vbon3g-WxZSzZC99bEtg',
+  },
+};
