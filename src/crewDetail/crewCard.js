a5658b83a579d134f637bd165a4560623f4b928d
CDR4733 <shsing2@naver.com>
2024-05-02 22:48:50 +0900

    [Update] - 감독 및 출연진 카드 리스트 추가

---
+++ src/crewDetail/crewCard.js
@@ -0,0 +1,54 @@
+import { fetchCreditData } from "../tmdb-api/api.js";
+
+// 0# 소개카드 만들기 총괄 수행 함수
+export const introduceCard = async () => {
+  const fetchData = await fetchCreditData();
+  const crew = fetchData["crew"]; // fetchData에서 제작진(crew)
+  const cast = fetchData["cast"]; // fetchData에서 출연진(cast)
+  const director = crew.filter((v) => {
+    if (v.job === "Director") {
+      return v; // 제작진(crew) 중에서 감독을 찾아서 할당
+    }
+  });
+  for (let i = 0; i < 6; i++) {
+    introduceCast(cast[i]); // 2번함수 실행
+    if (director.length > i) {
+      introduceDirector(director[i]); // 1번함수 실행
+    }
+  }
+};
+
+// 1# 감독(director) 소개카드 만들어서 붙이는 함수
+const introduceDirector = (director) => {
+  let profilePath = director.profile_path;
+  let name = director.name;
+  let department = director.department;
+  let directorCardHTML = `
+                <li class="director">
+                  <img src="https://www.themoviedb.org/t/p/w138_and_h175_bestv2${profilePath}" />
+                  <p class="creditName">${name}</p>
+                  <p class="character">${department}</p>
+                </li>
+        `;
+  const directorCard = document.getElementById("directorCard");
+  directorCard.insertAdjacentHTML("BeforeEnd", directorCardHTML);
+};
+
+// 2# 출연진(cast) 소개카드 만들어서 붙이는 함수
+const introduceCast = (cast) => {
+  let profilePath = cast.profile_path;
+  let name = cast.name;
+  let character = cast.character;
+  let castCardHTML = `
+                <li class="actor">
+                  <img src="https://www.themoviedb.org/t/p/w138_and_h175_bestv2${profilePath}" />
+                  <p class="creditName">${name}</p>
+                  <p class="character">${character}</p>
+                </li>
+        `;
+  const castCard = document.getElementById("actorCard");
+  castCard.insertAdjacentHTML("BeforeEnd", castCardHTML);
+};
+
+// 페이지가 시작되면 카드가 출력되도록
+introduceCard();
