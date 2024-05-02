454b365042fc91661f9bb08b89c0975bb78abfea
Eliot Jang <eliotjang2@gmail.com>
2024-05-02 09:34:13 +0900

    refactor - with .prettierrc

--- src/main.js
+++ src/main.js
@@ -1,16 +1,16 @@
-import { fetchData, searchData } from './tmdb-api/api.js';
+import { fetchData, searchData } from "./tmdb-api/api.js";
 import {
   setupPaginationButtons,
   updatePageNumbers,
-} from './pagination/pagination.js';
-import { renderMovieList, makeMovieList } from './render.js';
-import { searchFunc } from './search/search.js';
+} from "./pagination/pagination.js";
+import { renderMovieList, makeMovieList } from "./render.js";
+import { searchFunc } from "./search/search.js";
 //파일간에 상태유지를 위해서 선언.
 //import { state } from './state.js';
-import stateManager from './state.js';
+import stateManager from "./state.js";
 
 // 페이지 로드 시 초기 데이터 로딩과 페이지네이션 설정
-window.addEventListener('load', async () => {
+window.addEventListener("load", async () => {
   await loadPageData(); // 페이지 데이터 로드 및 렌더링
   setupPaginationButtons(); // 페이지네이션 버튼 설정
 });
@@ -24,10 +24,10 @@ const loadPageData = async () => {
   }
 };
 
-const $searchMovie = document.querySelector('.searchMovie');
-$searchMovie.addEventListener('submit', async (e) => {
+const $searchMovie = document.querySelector(".searchMovie");
+$searchMovie.addEventListener("submit", async (e) => {
   e.preventDefault();
-  const searchedString = e.target['search'].value.toUpperCase();
+  const searchedString = e.target["search"].value.toUpperCase();
   stateManager.updateState({ currentSearchQuery: searchedString });
   searchFunc();
 });
