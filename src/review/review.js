bdfbd57703c0c15d281240b86fb8c4a3e6e164be
Eliot Jang <eliotjang2@gmail.com>
2024-05-07 12:58:53 +0900

    [Update] - 리뷰 수정 기능

--- src/review/review.js
+++ src/review/review.js
@@ -11,13 +11,44 @@ const controlReview = () => {
     addLocalStorage();
   });
 
-  /*   const editBtn = document.querySelector(".editBtn");
-  editBtn.addEventListener("click", () => {
-    console.log("수정");
-  }); */
-
   printReview();
   deleteReview();
+  editReview();
+};
+
+const editReview = () => {
+  const editBtns = document.querySelectorAll(".editBtn");
+
+  editBtns.forEach((editBtn, index) => {
+    editBtn.addEventListener("click", () => {
+      openEditModal();
+
+      const editCheckBtn = document.querySelector(".editCheckBtn");
+      const reviewEditModal = document.querySelector(".reviewEditModal");
+
+      let editText = "";
+      let editRatingStar = "";
+      let editReviewPassword = "";
+      editCheckBtn.addEventListener("click", () => {
+        reviewEditModal.style.display = "none";
+        editText = document.querySelector(".editText").value;
+        editRatingStar = document.querySelector(".editRatingStar").value;
+        editReviewPassword = document.querySelector(".editReviewPassword").value;
+        console.log(editText);
+        console.log(editRatingStar);
+        console.log(editReviewPassword);
+
+        const writerRows = document.querySelectorAll(".writerRow");
+        let checkWriter = ``;
+        writerRows.forEach((e, i) => {
+          if (index === i) checkWriter = e.innerText;
+        });
+
+        console.log(checkWriter);
+        console.log(`${index}번 삭제`);
+      });
+    });
+  });
 };
 
 const deleteReview = () => {
@@ -34,7 +65,6 @@ const deleteReview = () => {
       deleteCheckBtn.addEventListener("click", () => {
         reviewDeleteModal.style.display = "none";
         deleteReviewPassword = document.querySelector(".deleteReviewPassword").value;
-        console.log(deleteReviewPassword);
 
         const writerRows = document.querySelectorAll(".writerRow");
         let checkWriter = ``;
@@ -60,7 +90,7 @@ let addTable = () => {
   reviewTable.innerHTML = `
   <table class="reviewTable">
     <tr>
-      <td><input class="reviewText" type="text" placeholder="리뷰 내용 (최대 30자)" size=60 maxlength=30 /></td>
+      <td><input class="reviewText" type="text" placeholder="리뷰 내용 (최대 30자)" size="60" maxlength="30" /></td>
       <td>
         <select class="ratingStar">
           <option value = "none">별점</option>
@@ -185,6 +215,26 @@ let openDeleteModal = () => {
   };
 
   clickDelete();
+};
+
+let openEditModal = () => {
+  const editCloseModal = document.querySelector(".editCloseModal");
+  const reviewEditModal = document.querySelector(".reviewEditModal");
+  const editCloseBtn = document.querySelector(".editCloseBtn");
+
+  editCloseModal.addEventListener("click", () => {
+    reviewEditModal.style.display = "none";
+  });
+
+  editCloseBtn.addEventListener("click", () => {
+    reviewEditModal.style.display = "none";
+  });
+
+  let clickEdit = () => {
+    reviewEditModal.style.display = "block";
+  };
+
+  clickEdit();
 };
 
 // 리뷰 작성 수행 함수 실행
